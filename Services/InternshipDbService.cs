ed665d8974229e0fabaf0b73a3d75e2465520db1
AzureAD\Vlad-TeodorGogu <vlad.gogu@principal33.com>
2021-04-19 13:31:40 +0300

    Added a default location so we can use it when adding a new member

--- Services/InternshipDbService.cs
+++ Services/InternshipDbService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.Extensions.Configuration;
 using RazorMvc.Data;
 using RazorMvc.Models;
 
@@ -9,17 +10,35 @@ namespace RazorMvc.Services
     public class InternshipDbService : IInternshipService
     {
         private readonly InternDbContext db;
+        private IConfiguration configuration;
+        private Location defaultLocation;
 
-        public InternshipDbService(InternDbContext db)
+        public InternshipDbService(InternDbContext db, IConfiguration configuration)
         {
             this.db = db;
+            this.configuration = configuration;
         }
 
         public Intern AddMember(Intern member)
         {
+            if(member.Location==null)
+            {
+                member.Location = GetDefaultLocation();
+            }
             db.Interns.AddRange(member);
             db.SaveChanges();
             return member;
+        }
+
+        private Location GetDefaultLocation()
+        {
+            if (defaultLocation == null)
+            {
+                var defaultLocationName = configuration["DefaultLocation"];
+                defaultLocation = db.Locations.Where(_ => _.Name == defaultLocationName).OrderBy(_ => _.Id).FirstOrDefault();
+            }
+
+            return defaultLocation;
         }
 
         public void EditMember(Intern intern)
