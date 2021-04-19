00579e91032dfcb707b99bf8af1428a7de585e06
AzureAD\Vlad-TeodorGogu <vlad.gogu@principal33.com>
2021-04-19 13:05:31 +0300

    Modified Seed data so it contains Location
    Showing Location only when we get a member by Id

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -15,14 +15,24 @@ namespace RazorMvc.Data
                 return;   // DB has been seeded
             }
 
+            Location defaultLocation;
+            var locations = new Location[]
+            {
+            defaultLocation = new Location { Name = "Kyiv", NativeName = "Київ", Longitude = 30.5167, Latitude = 50.4333, },
+            new Location { Name = "Brasov", NativeName = "Braşov", Longitude = 25.3333, Latitude = 45.75, },
+            };
+
+            context.Locations.AddRange(locations);
+
             var interns = new Intern[]
             {
-                new Intern { Name = "Borys", RegistrationDateTime = DateTime.Parse("2021-04-01") },
-                new Intern { Name = "Liova", RegistrationDateTime = DateTime.Parse("2021-04-01") },
-                new Intern { Name = "Orest", RegistrationDateTime = DateTime.Parse("2021-03-31") },
+                new Intern { Name = "Vlad", RegistrationDateTime = DateTime.Parse("2021-04-01"), Location = defaultLocation },
+                new Intern { Name = "Ale", RegistrationDateTime = DateTime.Parse("2021-04-01"), Location = defaultLocation },
+                new Intern { Name = "Borys", RegistrationDateTime = DateTime.Parse("2021-03-31"), Location = defaultLocation },
             };
 
             context.Interns.AddRange(interns);
+
             context.SaveChanges();
         }
     }
