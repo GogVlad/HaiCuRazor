741a56fabab6ec5c92eb9f2f6975aa1efe2a25e4
AzureAD\Vlad-TeodorGogu <vlad.gogu@principal33.com>
2021-04-28 11:11:10 +0300

    Implemented EmployeeController

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RazorMvc.Models;
+using RazorMvc.Services;
+
+namespace RazorMvc.Controllers
+{
+    [Route("employee/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmployeeDbService employeeService;
+
+        public EmployeeController(EmployeeDbService employeeService)
+        {
+            this.employeeService = employeeService;
+        }
+
+        // GET: employee/<EmployeeController>
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            return employeeService.GetEmployees();
+        }
+
+        // GET employee/<EmployeeController>/5
+        [HttpGet("{id}")]
+        public Employee Get(int id)
+        {
+            return employeeService.GetEmployeeById(id);
+        }
+
+        // POST employee/<EmployeeController>
+        [HttpPost]
+        public void Post([FromBody] Employee employee)
+        {
+            var newEmployee = employeeService.AddEmployee(employee);
+        }
+    }
+}
